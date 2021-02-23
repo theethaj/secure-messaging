a054dc52af34bf26758e191c3670460ff99f8309
theethaj <bank30827@gmail.com>
2021-02-23 18:55:54 +0700

    Change to 13

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder(13);
     }
 
     @Override
